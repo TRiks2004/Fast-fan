0d4c16e3a55bb6d2fbf15ad39c636b34e84ef729
Бурлаков Вениамин <test@rutoll.ru>
2025-04-17 11:22:40 +0300

    Обновление версии компонента Fast Fan с 0.0.1 до 0.0.2 в файле manifest.json. Удалены неиспользуемые импорты в файле switch.py для оптимизации кода.

--- custom_components/fast_fan/switch.py
+++ custom_components/fast_fan/switch.py
@@ -1,7 +1,5 @@
 from __future__ import annotations
-
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
 
 from .const import DOMAIN
 
