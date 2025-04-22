7e3a4094822aeb91ad635114e6332e4d4b661a19
Бурлаков Вениамин <test@rutoll.ru>
2025-04-22 23:57:53 +0300

    bagfix

--- custom_components/fast_fan/model/fan/zhimi/ZA5/ent.py
+++ custom_components/fast_fan/model/fan/zhimi/ZA5/ent.py
@@ -4,9 +4,12 @@ from custom_components.fast_fan.model.fan.zhimi.ZA5 import FanZhimiZA5, switch
 class FanEntity:
     def __init__(self, object: FanZhimiZA5) -> None:
         self.object = object
-        self.object._entities.append[
-            switch.FanPowerSwitch,
-        ]
+
+        entities = [switch.FanPowerSwitch,]
+        
+        for obj in entities:
+            self.object._entities.append(obj)
+        
         self.upload_entities()
 
     def upload_entities(self):
